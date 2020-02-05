6b101217f47a6035fb9813dbf3492052587fcafe
Brett Sheleski <brett@sheleski.com>
2020-02-05 07:09:44 -0600

    added hand pointer

---
+++ Prefabs/Cursor/Cursor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FVTC.LearningInnovations.Unity.OvrHelpers.Prefabs.Cursor
+{
+    public class Cursor : MonoBehaviour
+    {
+        
+        public virtual void Hide()
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        public virtual void Show(RaycastHit hit)
+        {
+            transform.position = hit.point;
+            transform.LookAt(hit.point + hit.normal);
+
+            this.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Prefabs/HandPointer/HandPointer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FVTC.LearningInnovations.Unity.OvrHelpers.Prefabs.HandPointer
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class HandPointer : MonoBehaviour
+    {
+        [SerializeField]
+        public Cursor.Cursor Cursor;
+
+        [SerializeField]
+        public float MaxDistance = 10f;
+
+        [SerializeField]
+        public LayerMask LayerMask;
+
+        [SerializeField]
+        public float PointerLength = 1;
+
+        LineRenderer _lineRenderer;
+
+        private void Start()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+
+            if (Cursor != null)
+            {
+                if (Cursor.gameObject.scene.name == null)
+                {
+                    // must be a prefab, create an instance
+
+                    Cursor.Cursor cursorInstance = Instantiate<Cursor.Cursor>(this.Cursor);
+                    cursorInstance.Hide();
+
+                    this.Cursor = cursorInstance;
+                }
+            }
+        }
+
+        private void Update()
+        {
+            _lineRenderer.SetPosition(0, this.transform.position);
+            _lineRenderer.SetPosition(1, this.transform.position + (this.transform.forward * PointerLength));
+
+            if (Cursor != null)
+            {
+                RaycastHit hit;
+
+                Ray ray = new Ray(this.transform.position, this.transform.forward);
+
+                if (Physics.Raycast(ray, out hit, MaxDistance, LayerMask))
+                {
+                    Cursor.Show(hit);
+                }
+                else
+                {
+                    Cursor.Hide();
+                }
+            }
+        }
+    }
+}
